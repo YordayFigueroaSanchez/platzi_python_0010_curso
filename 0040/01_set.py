38c8049fcc4909c66da4864155bbbe51d1889cf4
yfsanchez <yfsanchez@gmail.com>
2025-12-05 09:28:29 -0300

    conjuntos

---
+++ 0040/01_set.py
@@ -0,0 +1,13 @@
+set_countries = {'cuba','mexico','colombia','venezuela'}
+print(set_countries)
+print(type(set_countries))
+
+numbers = [11,2,1,2,3,4,5,6,5,6,7,8,9,10]
+print(numbers)
+print(type(numbers))
+set_numbers = set(numbers)
+print(set_numbers)
+print(type(set_numbers))
+list_numbers = list(set_numbers)
+print(list_numbers)
+print(type(list_numbers))
--- 0040/README.md
+++ 0040/README.md
@@ -1,1 +1,9 @@
 # [Curso Intermedio de Python: Manejo de Errores y Conceptos Avanzados](https://platzi.com/cursos/python-funciones/el-zen-de-python/)
+
+# [Conjuntos en Python: Propiedades y Uso Práctico](https://platzi.com/cursos/python-funciones/sets/)
+No permite duplicados
+No tiene un orden
+Permite modificarse
+
+[0040/01_set.py](01_set.py)
+
